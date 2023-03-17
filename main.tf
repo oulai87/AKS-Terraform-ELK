03840749c1273463bb2a0bac66c7ebcf43fa9ee9
Thomas <oulaithomas05@gmail.com>
2023-03-16 22:32:57 -0500

    update

--- main.tf
+++ main.tf
@@ -13,4 +13,19 @@ resource "azurerm_log_analytics_workspace" "azure_workspace" {
    name = "k8s-workspace-${random_id.azure_random.hex}"
    resource_group_name = azurerm_resource_group.azure_k8s.name
    sku = "PerGB2018"
+   retention_in_days = 30
+
 }
+
+resource "azurerm_log_analytics_solution" "azure_logsolution" {
+   location = azurerm_resource_group.azure_k8s.location
+   resource_group_name = azurerm_resource_group.azure_k8s.name
+   solution_name = "ContainerInsights"
+   workspace_name = azurerm_log_analytics_workspace.azure_workspace.name
+   workspace_resource_id = azurerm_log_analytics_workspace.azure_workspace.id
+   plan {
+     publisher = "Microsoft"
+     product = "OMSGallery/ContainerInsights"
+   }
+}
+
