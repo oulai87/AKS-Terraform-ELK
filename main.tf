d4d95576dca0dd1f3bbde044869062b52128a4d8
Thomas <oulaithomas05@gmail.com>
2023-03-17 07:24:50 -0500

    final commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "azure_k8s" {
   location = var.location
   name = local.common_name
-  tags = var.tag
+  tags = var.tags
 }
 
 resource "random_id" "azure_random" {
@@ -29,3 +29,74 @@ resource "azurerm_log_analytics_solution" "azure_logsolution" {
    }
 }
 
+resource "azurerm_virtual_network" "vnet" {
+   address_space = [element(var.address_space,0)]
+   location = var.location
+   name = "${local.common_name}-vnet"
+   resource_group_name = azurerm_resource_group.azure_k8s.name
+  
+}
+
+resource "azurerm_subnet" "subnet" {
+  address_prefix = element(var.address_space,1)
+  name = "${location.common_name}-subnet"
+  resource_group_name = azurerm_resource_group.azure_k8s.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  location = var.location
+  name = "${local.common_name}-public_ip"
+  resource_group_name = azurerm_resource_group.azure_k8s.name
+}
+
+resource "azurerm_kubernetes_cluster" "k8s_cluster" {
+  dns_prefix = var.dns_prefix
+  location = var.location
+  kubernetes_version = "1.24"
+  name = "${local.common_name}-k8scluster"
+
+  resource_group_name = azurerm_resource_group.azure_k8s.name
+  default_node_pool {
+    name = var.agent_pool
+    vm_size = var.agent_pool
+    node_count = 2
+    availability_zone = ["1","2"]
+  }
+
+  service_principal {
+    client_id = var.client_id
+    client_secret = var.client_secret
+  }
+
+  network_profile {
+    network_plugin = element(var.network_profile,0)
+    load_balancer_sku = element(var.network_profile,2)
+    network_policy = element(var.network_profile,0)
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  addon_profile {
+    oms_agent {
+      enabled = true
+      log_analytics_workspace_id = azurerm_log_analytics_solution.azure_logsolution.id
+    }
+
+    kube_dashboard {
+      enabled = true
+    }
+
+   lifecycle {
+     ignore_changes = [
+      windows_profile,
+        default_node_pool,
+     ] 
+
+    } 
+      
+    tags = var.tags
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,11 +1,40 @@
-variable "tag" {
-  default = ""
-}
 
 variable "location" {
-  default = ""
+  default = "eastus"
 }
 
 locals {
   common_name = "azure_k8s"
+}
+
+variable "address_space" {
+  default = ["10.1.0.0/16","10.1.0.0/24","10.1.0.0/32"]
+}
+
+variable "dns_prefix" {
+  default ="azurek8sdemo"
+}
+
+variable "agent_pool" {
+  default = ["defaultpool","Standard_D2s_v3"]
+}
+
+variable "client_secret" {
+  default =""
+}
+
+variable "client_id" {
+  default =""
+}
+
+variable "tags" {
+  default = {
+    terraform = "yes",
+    resource = "AKS",
+    purpose = "demo"
+  }
+}
+
+variable "network_profile" {
+   default = ["azure","kubenet","Standard"]
 }
